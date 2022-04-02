b390be8dc595add40a63f505d18989755907ecfd
csb@kde-neon <claudimards@gmail.com>
2022-04-02 15:01:29 -0300

    Added handleFavoriteChar function

--- src/components/HeroCard/index.tsx
+++ src/components/HeroCard/index.tsx
@@ -1,44 +1,60 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { RiShieldCrossLine } from 'react-icons/ri'
+import { BsShieldPlus, BsShieldMinus } from 'react-icons/bs'
 
 import styles from './styles.module.scss'
 
-type HeroProps = {
+type CharProps = {
   id: number;
   name: string;
   thumbnail: {
     extension: string;
     path: string;
-  }
+  };
 }
 
-type HeroType = {
-  hero: HeroProps;
+type Char = {
+  char: CharProps;
+  handleFavoriteChar: (char: {
+    id: number,
+    name: string,
+    thumbnail: {
+      extension: string;
+      path: string;
+    }
+  }) => void;
+  isFavorite?: boolean | undefined;
 }
 
-export const HeroCard = ({ hero }: HeroType) => {
+export const HeroCard = ({ char, handleFavoriteChar, isFavorite }: Char) => {
   return (
     <div className={styles.heroCard}>
-      <button type="button" title="Add to bookmark">
-        <RiShieldCrossLine />
+      <button type="button" title="Add to bookmark" onClick={() => handleFavoriteChar({
+        id: char.id,
+        name: char.name,
+        thumbnail: {
+          extension: char.thumbnail.extension,
+          path: char.thumbnail.path
+        }
+      })}>
+        {isFavorite ? ( <BsShieldMinus /> ) : ( <BsShieldPlus /> )}
       </button>
-      <Link href={`/character/${hero.id}`}>
+      <Link href={`/character/${char.id}`}>
         <a>
           <header className={styles.heroCardHeader}>
             <figure>
               <Image
-                src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
+                src={`${char.thumbnail.path}.${char.thumbnail.extension}`}
                 width="195"
                 height="210"
-                alt={hero.name}
+                alt={char.name}
               />
             </figure>
 
           </header>
           <article className={styles.heroCardContent}>
             <h2 className="heroName">
-              {hero.name}
+              {char.name}
             </h2>
           </article>
         </a>
--- src/components/HeroesList/index.tsx
+++ src/components/HeroesList/index.tsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { HeroCard } from '../HeroCard'
 import styles from './styles.module.scss'
 
@@ -8,7 +11,8 @@ type Char = {
   thumbnail: {
     extension: string;
     path: string;
-  }
+  };
+  isFavorite?: boolean | undefined;
 }
 
 type CharListTypes = {
@@ -17,15 +21,108 @@ type CharListTypes = {
 }
 
 export const HeroesList = ({ heroes, orderBy }: CharListTypes) => {
-  const [charList, setCharList] = useState(heroes)
+  const [charList, setCharList] = useState<Char[]>(heroes)
+  const [favoritesCharacters, setFavoritesCharacters] = useState<Char[]>([])
 
+  // FUNCTION TO HANDLE FAVORITE CHARS
+  const handleFavoriteChar = (char: Char) => {
+    // CHECK IF CHAR ALREADY IS IN FAVORITES
+    const alreadyFavorite = favoritesCharacters?.some(fav => {
+      return fav.id === char.id
+    })
+
+    // IF CHAR IS IN FAVORITE LIST
+    if ( alreadyFavorite ) {
+      // ASK IF WANT TO REMOVE CHAR FROM FAVORITE LIST
+      const confirmAnswer = confirm('Remove character from Favorites?')
+
+      if ( confirmAnswer ) {
+        const newFavoritesChars = favoritesCharacters.filter(currentChar => currentChar.id !== char.id).map(currentChar => {
+          return {
+            name: currentChar.name,
+            id: currentChar.id,
+            thumbnail: currentChar.thumbnail,
+            isFavorite: false
+          }
+        })
+
+        setFavoritesCharacters(newFavoritesChars)
+        toast.success('Character successfuly removed')
+
+        const newCharListState = charList.map((currentChar : Char) => {
+          if ( currentChar.id === char.id ) {
+            return {
+              ...currentChar,
+              isFavorite: !currentChar.isFavorite
+            }
+          }
+          return currentChar
+        })
+
+        setCharList(newCharListState)
+        return
+      }
+
+      toast.warning('Character was not removed')
+      return
+
+    } else {
+      // IF CHAR IS NOT IN FAVORITE LIST, ASK TO ADD
+      const confirmAnswer = confirm('Add character to Favorites?')
+
+      if (confirmAnswer) {
+        setFavoritesCharacters((prevState) => {
+          return [
+            ...prevState,
+            {
+              ...char,
+              isFavorite: true
+            }
+          ]
+        })
+
+        toast.success('Character successfuly added to Favorites!')
+
+        // CHANGE CHAR STATE FROM MAIN LIST
+        const newCharListState = charList.map((currentChar : Char) => {
+          if ( currentChar.id === char.id ) {
+            return {
+              ...currentChar,
+              isFavorite: !currentChar.isFavorite
+            }
+          }
+          return currentChar
+        })
+
+        setCharList(newCharListState)
+
+        // REMOVE FAVORITED CHARS FROM MAIN LIST
+        /* const newCharlist = charList.filter(currentChar => currentChar.id !== char.id).map(currentChar => {
+          return {
+              name: currentChar.name,
+              id: currentChar.id,
+              thumbnail: currentChar.thumbnail,
+              isFavorite: currentChar.isFavorite
+            }
+        })
+
+        setCharList(newCharlist) */
+        return
+      }
+
+      return toast.warning('Character not added!')
+    }
+  }
+
+  // EFFECT WHEN ORDER BY CHANGE
   useEffect(() => {
     if ( orderBy === 'nameDesc' ) {
       let newCharListOrder = charList.map((char: Char) => {
         return {
           name: char.name,
           id: char.id,
-          thumbnail: char.thumbnail
+          thumbnail: char.thumbnail,
+          isFavorite: char.isFavorite
         }
       })
 
@@ -41,7 +138,8 @@ export const HeroesList = ({ heroes, orderBy }: CharListTypes) => {
         return {
           name: char.name,
           id: char.id,
-          thumbnail: char.thumbnail
+          thumbnail: char.thumbnail,
+          isFavorite: char.isFavorite
         }
       })
 
@@ -56,12 +154,32 @@ export const HeroesList = ({ heroes, orderBy }: CharListTypes) => {
 
   return (
     <section className="container">
+      <ToastContainer />
+
+      {!!favoritesCharacters?.length && (
+        <>
+          <h3>My Favorites</h3>
+          <article className={styles.heroesList}>
+            {favoritesCharacters.map((char: Char) => (
+              <HeroCard
+                key={char.id}
+                char={char}
+                handleFavoriteChar={handleFavoriteChar}
+                isFavorite={char?.isFavorite}
+              />
+            ))}
+          </article>
+        </>
+      )}
+
       <article className={styles.heroesList}>
 
-        {charList.map(hero => (
+        {charList.map((char: Char) => (
           <HeroCard
-            key={hero.id}
-            hero={hero}
+            key={char.id}
+            char={char}
+            handleFavoriteChar={handleFavoriteChar}
+            isFavorite={char?.isFavorite}
           />
         ))}
 
